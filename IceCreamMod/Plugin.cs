35faa8a2f56e284864770a9c2ccf79b0ca25d7bb
nuggetcrab <johnswift@live.com>
2023-11-26 18:21:33 -0600

    Add files via upload

---
+++ IceCreamMod/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using BepInEx.Configuration;
+using GameNetcodeStuff;
+using IceCreamDrop.Patches;
+
+namespace IceCreamDrop
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ICDModBase : BaseUnityPlugin
+    {
+        const string modGUID = "Aries.IceCreamTruck";
+        const string modName = "Ice Cream Dropship";
+        const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ICDModBase Instance;
+
+        internal ManualLogSource val;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            val = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            val.LogWarning("ICD is online!");
+
+            harmony.PatchAll(typeof(ICDModBase));
+            harmony.PatchAll(typeof(SoundPatch));
+
+        }
+
+    }
+
+}
